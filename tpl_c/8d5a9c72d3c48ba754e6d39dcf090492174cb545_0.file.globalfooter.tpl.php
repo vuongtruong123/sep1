e9e8f05a5166bc059410f77a0c238e15ae2bcd04
Trịnh Như Phương <trinhnhuphuong.qn1@gmail.com>
2019-07-03 11:22:47 +0700

    frontend
    thong tin lien lac

---
+++ tpl_c/8d5a9c72d3c48ba754e6d39dcf090492174cb545_0.file.globalfooter.tpl.php
@@ -0,0 +1,61 @@
+<?php
+/* Smarty version 3.1.30, created on 2019-07-03 06:11:03
+  from "C:\xampp\htdocs\sep1\tpl\globalfooter.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5d1c2ad7d58ce1_18594358',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8d5a9c72d3c48ba754e6d39dcf090492174cb545' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\sep1\\tpl\\globalfooter.tpl',
+      1 => 1562118978,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d1c2ad7d58ce1_18594358 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+	</div><!-- close main-->
+
+	<footer class="footer navbar">
+		&copy; 2019 <a href="http://www.twinkletoessoftware.com">Twinkle Toes Software</a> <br/><a href="http://www.bookedscheduler.com">Booked Scheduler v<?php echo $_smarty_tpl->tpl_vars['Version']->value;?>
+</a>
+	</footer>
+
+	<?php echo '<script'; ?>
+ type="text/javascript">
+		init();
+		$.blockUI.defaults.css.border = 'none';
+		$.blockUI.defaults.css.top = '25%';
+
+	<?php echo '</script'; ?>
+>
+
+	<?php if (!empty($_smarty_tpl->tpl_vars['GoogleAnalyticsTrackingId']->value)) {?>
+		
+			<?php echo '<script'; ?>
+>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  
+			  ga('create', '<?php echo $_smarty_tpl->tpl_vars['GoogleAnalyticsTrackingId']->value;?>
+', 'auto');
+			  ga('send', 'pageview');
+			<?php echo '</script'; ?>
+>
+	<?php }?>
+
+	</body>
+</html><?php }
+}
