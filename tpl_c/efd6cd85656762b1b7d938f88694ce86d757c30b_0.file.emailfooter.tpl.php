61a7d1ce1539cfe5930c5a5e9a46c70530d5b832
nuanmi155 <nuanmi155@gmail.com>
2019-07-08 09:59:13 +0700

    css schedule
    css schedule

---
+++ tpl_c/efd6cd85656762b1b7d938f88694ce86d757c30b_0.file.emailfooter.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.30, created on 2019-07-08 04:24:24
+  from "C:\xampp\htdocs\sep1\tpl\Email\emailfooter.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5d22a958347151_41773544',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'efd6cd85656762b1b7d938f88694ce86d757c30b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\sep1\\tpl\\Email\\emailfooter.tpl',
+      1 => 1562551583,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d22a958347151_41773544 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+	</body>
+</html><?php }
+}
