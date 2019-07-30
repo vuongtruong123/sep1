4c215f404948147763f0aae3775248e743c6b8bf
Nhân <phamquocnhan147@gmail.com>
2019-07-30 15:42:05 +0700

    nhan00

--- config/config.php
+++ config/config.php
@@ -21,16 +21,16 @@ $conf['settings']['registration.notify.admin'] = 'false';
 $conf['settings']['inactivity.timeout'] = '30';
 $conf['settings']['name.format'] = '{first} {last}';
 $conf['settings']['css.extension.file'] = '';
-<<<<<<< HEAD
+
 
 $conf['settings']['disable.password.reset'] = 'true';
 $conf['settings']['home.url'] = 'view-schedule.php';
 
 
-=======
+
 $conf['settings']['disable.password.reset'] = 'false';
 $conf['settings']['home.url'] = 'dashboard.php';
->>>>>>> b951d7da1c60b153254df3efec6c6475c092bb55
+
 $conf['settings']['logout.url'] = 'view-schedule.php';
 $conf['settings']['default.homepage'] = '1';
 $conf['settings']['schedule']['use.per.user.colors'] = 'false';
@@ -74,7 +74,7 @@ $conf['settings']['uploads']['reservation.attachment.path'] = 'uploads/reservati
 $conf['settings']['uploads']['reservation.attachment.extensions'] = 'txt,jpg,gif,png,doc,docx,pdf,xls,xlsx,ppt,pptx,csv';
 $conf['settings']['database']['type'] = 'mysql';
 $conf['settings']['database']['user'] = 'booked_user';
-$conf['settings']['database']['password'] = 'password';
+$conf['settings']['database']['password'] = '';
 $conf['settings']['database']['hostspec'] = '127.0.0.1';
 $conf['settings']['database']['name'] = 'booked';
 $conf['settings']['phpmailer']['mailer'] = 'mail';
